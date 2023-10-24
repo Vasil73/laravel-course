6bcc322c8326e77bcdc53e551cd4949b67ea840f
vasily <vs300259@gmail.com>
2023-10-24 16:36:26 +0300

    Added views and routes

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,24 @@
+
+
+
+    <footer class="container-xxl">
+        <div>
+            <div class="d-flex flex-wrap justify-content-between align-items-center px-md-4 p-2 mb-3 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="/contacts">
+                        <p class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">Контакты</p>
+                    </a>
+                    <span class="mb-3 mb-md-0 text-body-secondary"><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">© 2023 Компания, Инк.</span></span></span>
+                </div>
+
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
+
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+{{--@extends('layouts.default');--}}
+
+
+<header class="container-xxl">
+    <div>
+        <div class="d-flex flex-column flex-md-row align-items-center bg-dark text-white px-md-4 p-3 mb-3 border-bottom">
+            <h3 class="my-0 mr-md-auto font-weight-normal text-white">VsShubin</h3>
+            <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                <a class="me-3 py-2 link-body-emphasis text-white text-decoration-none" href="/">Главная</a>
+                <a class="me-3 py-2 link-body-emphasis text-white text-decoration-none" href="/contacts">Контакты</a>
+            </nav>
+        </div>
+    </div>
+</header>
