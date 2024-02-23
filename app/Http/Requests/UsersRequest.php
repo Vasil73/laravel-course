969f790b9aa6cf5d1a8ebffd74bf43ab19518431
vasily <vs300259@gmail.com>
2024-02-23 20:39:36 +0300

    new task

---
+++ app/Http/Requests/UsersRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UsersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|not_only_whitespace|min:3|max:50|',
+            'surname' => 'required|not_only_whitespace|min:2|max:50|unique:surname',
+            'email' => 'required|email|unique:users|regex:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Не забывайте указать имя',
+            'surname.required' => 'Без фамилии ни как',
+            'name.min' => 'Имя должно быть длиннее',
+            'surname.min' => 'Должно быть больше трех букв'
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,19 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\UserPdfController;
 
 //
 //Route::get('/employee', [EmployeeController::class, 'index'])->name ('index');
 //Route::post ('/employee/store', [EmployeeController::class, 'store'])->name ('index.edit');
 //Route::post('/employee/update/{id}', [EmployeeController::class, 'update']);
 
-Route::get('/books', [BookController::class, 'index'])->name ('books-index');
-Route::post('/books/store', [BookController::class, 'store'])->name ('books-name');
+//Route::get('/books', [BookController::class, 'index'])->name ('books-index');
+//Route::post('/books/store', [BookController::class, 'store'])->name ('books-name');
+
+Route::get ('/user/', [UsersController::class, 'index']);
+Route::get ('/user/{id}/', [UsersController::class, 'getUserId']);
+Route::post ('/user/create/', [UsersController::class, 'createUser'])->name ('user-create');
+Route::delete ('user/delete', [UsersController::class, 'userDelete']);
+Route::get ('user/resume-pdf', [UserPdfController::class, 'indexPdf']);
